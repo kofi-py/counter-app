9d3cd5901c0f44dec139b72b31b45789df7f44cd
kofi-py <79289050+kofi-py@users.noreply.github.com>
2021-07-22 13:01:11 -0400

    Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,8 +4,14 @@ class Counter extends Component {
     state = {
         count: 0
     };
+
     render() { 
-       return <div><span>{this.formatCount()}</span><button>Increment</button></div>;
+       return (
+        <div>
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>
+       );
     }
 
     formatCount() {
