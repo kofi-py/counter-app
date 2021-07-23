db2374eb74d96ff95e4c66179945e7cce5fd256b
kofi-py <79289050+kofi-py@users.noreply.github.com>
2021-07-23 14:09:11 -0400

    Passing Event Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,8 @@ class Counter extends Component {
     };
 
 
-    handleIncrement = () => {
+    handleIncrement = product => {
+        console.log(product);
         this.setState({ count: this.state.count + 1 });
     };
 
@@ -14,7 +15,7 @@ class Counter extends Component {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement({ id: 1})} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
 
