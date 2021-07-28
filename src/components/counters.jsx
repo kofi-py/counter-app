ad78d2d64c427f051cae13e35b6cc9b515187ce3
kofi-py <79289050+kofi-py@users.noreply.github.com>
2021-07-28 09:46:40 -0400

    Remove local state.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,17 +4,41 @@ import Counter from './counter';
 class Counters extends Component {
     state = {
         counters: [
-            { id: 1, value: 0 },
+            { id: 1, value: 4 },
             { id: 2, value: 0 },
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
     };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+       const counters = this.state.counters.map(c => {
+         c.value = 0;
+         return c;  
+       });
+
+       this.setState({ counters });
+    };
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters: counters });
+    };
+    
     render() { 
         return (
         <div>
+            <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
             {this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value}  />)}
+            <Counter key={counter.id} onDelete={this.handleDelete} onIncrement={this.handleIncrement} counter={counter}/>)}
         </div>
         );
     }
