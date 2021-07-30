dfbbe56dbb8f310b1929db734daa2eed998757da
kofi-py <79289050+kofi-py@users.noreply.github.com>
2021-07-30 11:11:29 -0400

    Destructing Arguments.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,12 +3,14 @@ import Counter from './counter';
 
 class Counters extends Component {
     
-    render() { 
+    render() {
+        const { onReset, counters, onDelete, onIncrement } = this.props;
+
         return (
         <div>
-            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.props.counters.map(counter => 
-            <Counter key={counter.id} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter}/>)}
+            <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            {counters.map(counter => 
+            <Counter key={counter.id} onDelete={onDelete} onIncrement={onIncrement} counter={counter}/>)}
         </div>
         );
     }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 // Stateless Functional Component
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
       return (
       <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar{""} <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+        Navbar{""} <span className="badge badge-pill badge-secondary">{totalCounters}</span>
       </a>
     </nav>
     );
