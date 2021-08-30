4ff10d51e8ad2d07b274fd717cc7cb373c8e00a4
kofi-py <79289050+kofi-py@users.noreply.github.com>
2021-08-30 12:33:09 -0400

    Final Touches.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Stateless Functional Component
 const NavBar = ({ totalCounters }) => {
       return (
       <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="https://reactjs.org/">
         Navbar{""} <span className="badge badge-pill badge-secondary">{totalCounters}</span>
       </a>
     </nav>
